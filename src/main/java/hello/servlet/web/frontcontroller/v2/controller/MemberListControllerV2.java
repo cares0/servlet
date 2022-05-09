c8d3036b24f75aea67aaa374315f9bdc2e6525fb
cares0 <cares00000@gmail.com>
2022-05-09 21:13:36 +0900

    프론트 컨트롤러 View 분리 - v2

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.basic.domain.member.Member;
+import hello.servlet.basic.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 비즈니스 로직 실행
+        List<Member> members = memberRepository.findAll();
+
+        // Model에 담기
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
